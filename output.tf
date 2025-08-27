5a26fd264ce63516b954617587b9ab041326c15c
ymphyow@gmail.com <ymphyow@gmail.com>
2025-08-26 21:59:13 -0400

    add s3 bucket ticket3456

--- output.tf
+++ output.tf
@@ -1,7 +1,7 @@
-output "Public-ip"{
-    value = aws_instance.server.public_ip
+output "Public-ip" {
+  value = aws_instance.server.public_ip
 }
 
-output "vpc-id"{
-    value = aws_vpc.my-vpc.id
+output "vpc-id" {
+  value = aws_vpc.my-vpc.id
 }
